c2667bc454acd51a71b52969b38ee0e5fd45b6f6
markers16123 <markers16123@gmail.com>
2018-07-18 18:04:17 +0900

    Apolo 클라이언트

--- apollo-react-app/src/App.js
+++ apollo-react-app/src/App.js
@@ -1,32 +1,21 @@
 import React, { Component } from 'react';
-import { Helmet } from 'react-helmet';
 import { HashRouter as Router, Route } from 'react-router-dom';
 import { ApolloProvider } from 'react-apollo';
 import client from './apolloClient';
 import Home from './Home';
-import Details from './Details';
+import Detail from './Detail';
 
 class App extends Component {
     render() {
         return (
-            <React.Fragment>
-                <Helmet>
-                    <meta charset="utf-8" />
-                    <title>My Title</title>
-                    <link rel="canonical" href="http://mysite.com/example" />
-                </Helmet>
-                <ApolloProvider client={client}>
-                    <Router>
-                        <React.Fragment>
-                            <Route exact={true} path={'/'} component={Home} />
-                            <Route
-                                path={'/details/:movieid'}
-                                component={Details}
-                            />
-                        </React.Fragment>
-                    </Router>
-                </ApolloProvider>
-            </React.Fragment>
+            <ApolloProvider client={client}>
+                <Router>
+                    <main>
+                        <Route exact={true} path={'/'} component={Home} />
+                        <Route path={'/details/:movieId'} component={Detail} />
+                    </main>
+                </Router>
+            </ApolloProvider>
         );
     }
 }
